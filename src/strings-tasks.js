03ccbeda43f7329101a5fac4479f9b1489bad1f3
ViacheslavAnisimov <forvsn@gmail.com>
2023-11-18 07:23:11 +0700

    feat: solved new tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -149,8 +149,12 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const idx = str.indexOf(value);
+  if (idx !== -1) {
+    return str.slice(0, idx) + str.slice(idx + value.length);
+  }
+  return str;
 }
 
 /**
@@ -165,8 +169,12 @@ function removeFirstOccurrences(/* str, value */) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const idx = str.lastIndexOf(value);
+  if (idx !== -1) {
+    return str.slice(0, idx) + str.slice(idx + value.length);
+  }
+  return str;
 }
 
 /**
